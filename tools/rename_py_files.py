e88773753bed74457911db8980ccab1e6ab1c51b
zxs1215 <ujjfvswc@gmail.com>
2015-11-22 09:54:24 +0800

    add rename script description

--- tools/rename_py_files.py
+++ tools/rename_py_files.py
@@ -2,7 +2,17 @@ import os
 import re
 
 """
-    rename files  replace ' ' to '_'  and replace '-' to '_'
+    I use the titile to name the py files,
+    Such as "292 Nim Game.py" 
+
+    when I run i in terminal as "python 292 Nim Game.py", 
+    it errors!
+    cause of the spaces.
+
+    It's trouble me a lot .
+
+    So I create this script to rename files,
+    replace ' ' to '_' .
 """
 os.chdir('..')
 
@@ -11,7 +21,7 @@ files = os.listdir('.')
 pys = [f for f in files if re.match(r'^\d.*py$',f)]
 
 for py in pys:
-    py_r = py.replace(' ','_').replace('-','_')
+    py_r = py.replace(' ','_')
     if py_r != py:
         os.rename(py, py_r)
 

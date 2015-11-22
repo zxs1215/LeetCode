574f8cfbd8e313c535296f325d1864978731ba18
zxs1215 <ujjfvswc@gmail.com>
2015-11-22 09:35:22 +0800

    rename ,replace '-' to '_'

--- tools/rename_py_files.py
+++ tools/rename_py_files.py
@@ -2,14 +2,16 @@ import os
 import re
 
 """
-    rename files  replace ' ' to '_' 
+    rename files  replace ' ' to '_'  and replace '-' to '_'
 """
+os.chdir('..')
 
-files = os.listdir('..')
+files = os.listdir('.')
 
 pys = [f for f in files if re.match(r'^\d.*py$',f)]
 
 for py in pys:
-    py_r = py.replace(' ','_')
-    os.rename(py, py_r)
+    py_r = py.replace(' ','_').replace('-','_')
+    if py_r != py:
+        os.rename(py, py_r)
 
